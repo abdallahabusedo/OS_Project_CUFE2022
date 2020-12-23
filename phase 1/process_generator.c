2efdfb48991b829b842e7bd00a954d9331648994
posidoon <abozoooz2014.aa@gmail.com>
2020-12-23 06:01:16 -0800

    Ask the user for the chosen scheduling algorithm

--- phase 1/process_generator.c
+++ phase 1/process_generator.c
@@ -1,13 +1,37 @@
 #include "headers.h"
-
+#define HPF 1
+#define SRTN 2
+#define RR 3
 void clearResources(int);
 
-int main(int argc, char * argv[])
+int selectedAlgorithmNumbers(int selected_algorithm_Number)
+{
+    if (selected_algorithm_Number != 1 ||
+        selected_algorithm_Number != 2 ||
+        selected_algorithm_Number != 3)
+    {
+        printf("Please select one of the following algorithms numbers 1 , 2 or 3");
+        scanf("%d", selected_algorithm_Number);
+        selectedAlgorithmNumbers(selected_algorithm_Number);
+    }
+    else
+    {
+        return selected_algorithm_Number;
+    }
+}
+int main(int argc, char *argv[])
 {
     signal(SIGINT, clearResources);
     // TODO Initialization
     // 1. Read the input files.
     // 2. Ask the user for the chosen scheduling algorithm and its parameters, if there are any.
+    int selected_algorithm_Number;
+    printf("Please select one of these algorithms: \n");
+    printf("1) Non-preemptive Highest Priority First (HPF).\n");
+    printf("2) Shortest Remaining time Next (SRTN).\n");
+    printf("3) Round Robin (RR).\n");
+    scanf("%d", selected_algorithm_Number);
+    selectedAlgorithmNumbers(selected_algorithm_Number);
     // 3. Initiate and create the scheduler and clock processes.
     // 4. Use this function after creating the clock process to initialize clock
     initClk();

c2f3a04916c34bee23793770e3c27fb90276fe9e
khalidalics22 <khaled.ali.06.10@gmail.com>
2020-12-23 17:35:53 +0200

    read processes from text file

--- phase 1/process_generator.c
+++ phase 1/process_generator.c
@@ -1,9 +1,45 @@
 #include "headers.h"
-
+#include <stdio.h>
+#include <string.h>
 #define HPF 1
 #define SRTN 2
 #define RR 3
+
 void clearResources(int);
+const char* PROCESSES_FILE_NAME = "phase 1/processes.txt";
+
+struct Process{
+    int id; 
+    int arrive; 
+    int runtime; 
+    int priority; 
+}; 
+
+struct Process ** readProcesses(){
+    FILE * file = fopen(PROCESSES_FILE_NAME,"r"); 
+    if(!file){
+        printf("not able to read processes"); 
+        exit(-1);                                   // what should we do here ? 
+    }
+    char * line; 
+    struct Process* processes[__INT8_MAX__];
+    int i = 0; 
+    size_t buf_size = 0; 
+    while(getline(&line,&buf_size,file) > 0){
+        if(line[0]=='#') continue; 
+        processes[i] = (struct Process *) malloc(sizeof(struct Process*));
+        processes[i]->id = line[0]-'0'; 
+        processes[i]->arrive = line[4]-'0'; 
+        processes[i]->runtime = line[8]-'0'; 
+        processes[i]->priority = line[12]-'0'; 
+        i++; 
+        // loop through the string to extract all other tokens
+       
+    }
+    fclose(file); 
+    return processes; 
+}
+
 
 int readAlgoNum()
 {
@@ -28,8 +64,8 @@ int main(int argc, char *argv[])
     signal(SIGINT, clearResources);
     // TODO Initialization
     // 1. Read the input files.
+    struct Process ** processes = readProcesses(); 
     // 2. Ask the user for the chosen scheduling algorithm and its parameters, if there are any.
-
     int selected_algorithm = readAlgoNum();
     // 3. Initiate and create the scheduler and clock processes.
     // 4. Use this function after creating the clock process to initialize clock

8ac45327802442d24d080e52e732b4126ca01db0
khalidalics22 <khaled.ali.06.10@gmail.com>
2020-12-24 17:35:39 +0200

    hope it works:)

--- phase1/process.c
+++ phase1/process.c
@@ -3,18 +3,44 @@
 /* Modify this file as needed*/
 int remainingtime;
 
+void set_remain_for_RR(int remain)
+{
+    int shmid, pid;
+    shmid = shmget(IPC_PRIVATE, 4096, IPC_CREAT | 0644);
+    int *shmaddr = (int*) shmat(shmid, (void *)0, 0);
+    if (*shmaddr == -1)
+    {
+        perror("Error in attach in writer");
+        exit(-1);
+    }
+    else
+    {
+        *shmaddr = remain; 
+    } 
+    shmdt(shmaddr);
+}
 int main(int agrc, char * argv[])
 {
     initClk();
-    
+    remainingtime = atoi(argv[1]); 
     //TODO it needs to get the remaining time from somewhere
     //remainingtime = ??;
+    if(agrc > 2){
+       int remain = atoi(argv[2]); 
+       if(remain < remainingtime){
+           remainingtime = remainingtime; 
+       }
+       remain = remain - remainingtime; 
+        set_remain_for_RR(remain);
+    }
+    printf("process started at time %d \n",getClk());
     while (remainingtime > 0)
     {
-        // remainingtime = ??;
+        remainingtime =-1;
+        sleep(1); 
     }
-    
+    printf("process ended at time %d  \n\n",getClk());
     destroyClk(false);
-    
+    kill(getppid(),SIGCHLD); 
     return 0;
 }
--- phase1/scheduler.c
+++ phase1/scheduler.c
@@ -1,13 +1,13 @@
 #include "headers.h"
 #include "priority_queue.h"
-char selAlgo; 
+#define RR_PERIOD 2
 void insert_HPF(struct Process p){
     struct Process *pp = (struct Process *) malloc(sizeof(struct Process)); 
     *pp = p ; 
     proirity_enqueue(pp,p.priority);
 }
-void forkProcess(int Q_size);
-
+void forkProcess();
+void switchProcess();
 void insert_SRTN(struct Process p){
     struct Process *pp = (struct Process *) malloc(sizeof(struct Process )); 
     *pp = p ; 
@@ -19,9 +19,12 @@ void insert_RR(struct Process p){
     proirity_enqueue(pp,p.arrive);
 }
 struct Process ** queue;
-int NextProcess = 0; 
+int size;
+char selAlgo; 
+struct Process *curr_process;
 int main(int argc, char * argv[])
 {
+    signal(SIGCHLD,switchProcess);
     selAlgo = *argv[1]; 
     key_t key_id;
     int rec_val, msgq_id;
@@ -35,6 +38,9 @@ int main(int argc, char * argv[])
     struct msgbuff message;
     initClk();
     queue = get_Q(); // initialize queue
+    int shmid, pid;
+    shmid = shmget(IPC_PRIVATE, 4096, IPC_CREAT | 0644);
+    bool first = true; 
     while (true)
     { 
         rec_val = msgrcv(msgq_id, &message, sizeof(message.p), G_MSG_TYPE, IPC_NOWAIT);
@@ -54,30 +60,75 @@ int main(int argc, char * argv[])
             }   
             
         }
-        int size = getQueueSize();
-        if(size>0)
-            forkProcess(size);
+        size = getQueueSize();
+        if(size>0&& first){
+            forkProcess();
+            first = false; 
+        }
 
     }
     //TODO implement the scheduler :)
     //upon termination release the clock resources.
     destroyClk(true);
 }
-void forkProcess(int Q_size){
-    // if current process is working do nothing 
-    //else if it is finished (i guess process will send it int signal)fork the next process 
-    switch (selAlgo)
+int getRemainOfLast()
+{
+    int shmid, pid;
+    shmid = shmget(IPC_PRIVATE, 4096, IPC_CREAT | 0644);
+    if (shmid == -1)
     {
-        case SRTN: 
-            //
-            
-            break;
-        case HPF: 
-            // when finished only
-            
-            break;
-        default:
-                
-            break;
-    }   
+        perror("Error in create");
+        exit(-1);
+    }
+    int *shmaddr = (int *) shmat(shmid, (void *)0, 0);
+    if (*shmaddr == -1)
+    {
+        perror("Error in attach in reader");
+        exit(-1);
+    }
+    int remain = (*shmaddr); 
+    shmctl(shmid, IPC_RMID, (struct shmid_ds *)0);
+    return remain   ; 
+   
 }
+void switchProcess(){
+    int remainOfLast = getRemainOfLast(); 
+    if(selAlgo == RR) {
+        if(remainOfLast != 0){
+            curr_process->remain = remainOfLast; 
+            proirity_enqueue(curr_process,curr_process->remain);
+        }
+    }
+    if(getQueueSize()>0){
+        forkProcess(); 
+    }
+}
+
+void forkProcess(){
+    int sch_pid = fork(),sch_stat_loc;
+    if (sch_pid == -1)
+        perror("error in fork");
+    else if (sch_pid == 0)
+    { 
+        int remaining_time ; 
+        curr_process  = dequeue(); 
+        switch (selAlgo)
+        {
+            case RR: 
+                remaining_time = RR_PERIOD;  
+                break;
+            default:
+                remaining_time = curr_process->runtime; 
+                break;
+        }
+        //printf("\nI am the child, my pid = %d and my parent's pid = %d\n\n", getpid(), getppid());
+        printf("start process with arrival %d at time %d \n\n",curr_process->arrive,getClk());
+        if(RR == selAlgo)
+            execl("/home/khalid/OS/OS_Project_CUFE2022/phase1/p.o", "p.o",remaining_time,curr_process->remain, NULL);
+        else{
+            execl("/home/khalid/OS/OS_Project_CUFE2022/phase1/p.o", "p.o",remaining_time, NULL);
+        }
+    }
+}
+   
+
